ebab4b8dd38f7eb322de7cb91b153b86c0f2cf6a
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-07-04 01:53:03 -0500

    added a logo on navbar banner

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -14,7 +14,7 @@ function Home() {
 
                         <p>I am a front-end developer based in Dallas, Texas. I’m passionate about learning the latest and greatest technologies whether that’s the front-end or back-end. I enjoy creating sleek, responsive and user friendly webpages. I am currently open to opportunities and if you feel like we would be a great match, I’d love to hear from you!</p><br />
                         <Link to={ `/projects` }><Button className="btn btn-ghost btn-lg mr-3">Projects</Button></Link>
-                        <Link to={ `/contact` }><Button className="btn btn-ghost btn-lg">Get in touch</Button></Link>
+                        <Link to={ `/contact` }><Button className="btn btn-ghost btn-lg">Get In Touch</Button></Link>
                     </div>
                 </div>
             </div>
