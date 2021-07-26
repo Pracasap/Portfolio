0a55f240bf0d04586c81424a8474f00c99bc3594
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-07-26 00:06:23 -0500

    fetch data from the server

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,3 +1,4 @@
 export const PROJECTS_LOADING = 'PROJECTS_LOADING';
 export const PROJECTS_FAILED = 'PROJECTS_FAILED'
-export const ADD_PROJECTS = 'ADD_PROJECTS'; 
+export const ADD_PROJECTS = 'ADD_PROJECTS';
+
