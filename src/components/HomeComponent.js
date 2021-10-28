4acb5275944eb446b15d463e5fc495b9c4065637
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-10-28 15:52:06 -0500

    Changed my domain name to psapkota.com

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -10,7 +10,7 @@ function Home() {
                     <div className="col-md-12 pt-4 pt-sm-4">
                         <h1>Hi, I am Prakash!</h1>
                         
-                        <h4 id="homeH4">I create beaufiful and responsive websites</h4><br></br>
+                        <h4 id="homeH4">I create beautiful and responsive websites</h4><br></br>
 
                         <p className="homeParagraph">I am a front-end developer based in Dallas, Texas. I’m passionate about learning the latest and greatest technologies whether that’s the front-end or back-end. I enjoy creating sleek, responsive and user friendly webpages. I am currently open to opportunities and if you feel like we would be a great match, I’d love to hear from you!</p><br />
                         <div className="mobileCenter"><Link to={ `/projects` }><Button className="btn btn-ghost btn-lg mr-3 mobileCenter">Projects</Button></Link>
